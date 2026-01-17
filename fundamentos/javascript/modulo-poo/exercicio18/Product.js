b1c7f5e61379cd7a006b9eb6bddf43844d06f8fe
Cleverton Inacio <cleverton.inacio@ufrn.br>
2026-01-16 21:26:02 -0300

    organiza código

--- fundamentos/javascript/modulo-poo/exercicio18/Product.js
+++ fundamentos/javascript/modulo-poo/exercicio18/Product.js
@@ -5,13 +5,15 @@ export default class Product {
     this.price = price;
     this.inStock = 0;
   }
+
   addToStock(value) {
     this.inStock += value;
   }
+
   calculateDiscount(percentage) {
     const valueWithDiscount = this.price * (1 - percentage / 100);
     console.log(
-      `O valor com ${percentage}% de desconto é: ${valueWithDiscount}`
+      `O valor com ${percentage}% de desconto é: ${valueWithDiscount}`,
     );
   }
 }
